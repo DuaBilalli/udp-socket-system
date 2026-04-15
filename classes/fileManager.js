99145391b1a60abe4fb1747822896a69f2082d31
Dreni02 <dren.kelmendi2@student.uni-pr.edu>
2026-04-15 09:14:35 +0200

    Update fileManager.js
    Add processCommand method for parsing commands and handling responses

--- classes/fileManager.js
+++ classes/fileManager.js
@@ -10,4 +10,11 @@ class FileManager {
       fsSync.mkdirSync(this.dir);
     }
   }
+  async processCommand(text, role, serverManager, rinfo) {
+    const parts = text.split(' ');
+    const command = parts[0];
+    const argument = parts.slice(1).join(' ');
+
+    const send = (msg) => serverManager.send(msg, rinfo.port, rinfo.address);
   }
+}
