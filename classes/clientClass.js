4490d03dbfee1311e58582e881b59eb01a31e38d
drennitaj06 <dren.nitaj@student.uni-pr.edu>
2026-04-17 23:01:24 +0200

    feat: add CLI interface using readline for user interaction

--- classes/clientClass.js
+++ classes/clientClass.js
@@ -1,10 +1,29 @@
 const dgram = require('dgram');
+const readline = require('readline');
 const config = require('../config/config');
 
 class UDPClient {
   constructor(role = 'user') {
     this.client = dgram.createSocket('udp4');
     this.role = role;
+    this.prefix = role === 'admin' ? 'ADMIN' : 'USER';
+
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      prompt: `${this.prefix}> `
+    });
+  }
+
+  start() {
+    console.log(`U lidh me serverin ${config.host}:${config.port}`);
+    this.rl.prompt();
+
+    this.rl.on('line', (line) => {
+      const input = line.trim();
+      if (input) this.send(input);
+      this.rl.prompt();
+    });
   }
 
   send(text) {
